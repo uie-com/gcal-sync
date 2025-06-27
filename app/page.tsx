bd51da051eaf8c4f21c990b4034619e5fe1d2fa5
alextyang <koopakondra@gmail.com>
2025-06-27 10:00:52 -0400

    Added suspense boundary

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function Home() {
+  return (
+    <Suspense fallback={
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-lg text-gray-600">Syncing...</p>
+      </div>
+    }>
+      <Body />
+    </Suspense>
+  )
+}
+
+function Body() {
   const params = useSearchParams();
 
   useEffect(() => {
-
     fetch('/sync?' + params, { method: 'POST', });
   }, []);
 
@@ -16,5 +27,5 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-4">Sync in Progress</h1>
       <p className="text-lg text-gray-600">You can now close this tab.</p>
     </div>
-  )
+  );
 }
